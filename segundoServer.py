d9c5373217ea696a021906e67e6852f52226195a
arsaca2122daw2 <arsaca2122daw2@protonmail.com>
2022-04-28 18:30:18 +0200

    Motores hacia alante, atras 0.3s / Matriz entera con colores

--- segundoServer.py
+++ segundoServer.py
@@ -1,11 +1,8 @@
 from buildhat import *
 from flask import *
-
+import time
 
 app = Flask(__name__)
-
-motorA = Motor('A')
-motorB = Motor('B')
 
 @app.route('/')
 def index():
@@ -17,17 +14,39 @@ def action(deviceName, action):
 
     if action == 'on':
         if deviceName == 'motor1':
-            motorA.run_for_seconds(1000)
+            print('Motor A adelante')
+            Motor('A').run_for_seconds(1000,100) #Endavant infinit MOTOR1
         if deviceName == 'motor2':
-            motorB.run_for_seconds(1000)
+            print('Motor B adelante')
+            Motor('B').run_for_seconds(0.3,100) #Endavant poc MOTOR2
 
     if action == 'off':
         if deviceName == 'motor1':
-            motorA.stop()
+            print('Motor A atrás')
+            Motor('A').run_for_seconds(0.3,-100) #Endarrere poc MOTOR1
         if deviceName == 'motor2':
-            motorB.stop()
-     
-    return render_template('index2.html') 
+            print('Motor B atrás')
+            Motor('B').run_for_seconds(0.3,-100) #Endarrere poc MOTOR2
+
+    return render_template('index2.html')
+
+#Matriz
+@app.route("/matriu", methods=["GET", "POST"])
+def sign_up():
+
+    if request.method == "POST":
+
+        matrix =  Matrix('C')
+
+        req = request.form
+        cella = req.get("cella")
+        color = req.get("color") 
+
+        print(color)
+        for x in range(100):
+            matrix.clear((color, 10))
+
+    return render_template("index2.html")
 
 if __name__ == '__main__':
    app.run(debug=True, port=5000, host='0.0.0.0')
