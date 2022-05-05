cad16ce803b874f9e3c7eb382997b00ab1a5561e
arsaca <arsaca2122daw2@protonmail.com>
2022-05-05 19:01:31 +0200

    CSS casi acabado, cambiar titulos Motores, live sincronizado con la camara

--- static/js/socketioClient.js
+++ static/js/socketioClient.js
@@ -18,7 +18,8 @@ window.onload = function() {
             apagarLuces();
             defaultTeclas();
             document.getElementById("luzW").src="../static/imgs/luzVerdeOn.png";
-            document.getElementById("letraW").style.backgroundColor="yellow";
+            document.getElementById("letraW").style.border="none";
+            document.getElementById("letraW").style.backgroundColor="rgb(131, 131, 131)";
             socket.emit("enviarKeyCode", keyCode)
         }
         else if(keyCode == 83){ //Key s
@@ -26,7 +27,8 @@ window.onload = function() {
             apagarLuces();
             defaultTeclas();
             document.getElementById("luzS").src="../static/imgs/luzVerdeOn.png";
-            document.getElementById("letraS").style.backgroundColor="yellow";
+            document.getElementById("letraS").style.border="none";
+            document.getElementById("letraS").style.backgroundColor="rgb(131, 131, 131)";
             socket.emit("enviarKeyCode", keyCode)
         }
         else if(keyCode == 81){ //Key q
@@ -34,7 +36,8 @@ window.onload = function() {
             apagarLuces();
             defaultTeclas();
             document.getElementById("luzQ").src="../static/imgs/luzVerdeOn.png";
-            document.getElementById("letraQ").style.backgroundColor="yellow";
+            document.getElementById("letraQ").style.border="none";
+            document.getElementById("letraQ").style.backgroundColor="rgb(131, 131, 131)";
             socket.emit("enviarKeyCode", keyCode)
         }
         else if(keyCode == 69){ //Key e
@@ -42,7 +45,26 @@ window.onload = function() {
             apagarLuces();
             defaultTeclas();
             document.getElementById("luzE").src="../static/imgs/luzVerdeOn.png";
-            document.getElementById("letraE").style.backgroundColor="yellow";
+            document.getElementById("letraE").style.border="none";
+            document.getElementById("letraE").style.backgroundColor="rgb(131, 131, 131)";
+            socket.emit("enviarKeyCode", keyCode)
+        }
+        else if(keyCode == 65){
+            console.log("Cliente pulsa A")
+            apagarLuces();
+            defaultTeclas();
+            document.getElementById("flechaIzq").src="../static/imgs/flechasON.png";
+            document.getElementById("letraA").style.border="none";
+            document.getElementById("letraA").style.backgroundColor="rgb(131, 131, 131)";
+            socket.emit("enviarKeyCode", keyCode)
+        }
+        else if(keyCode == 68){
+            console.log("Cliente pulsa D")
+            apagarLuces();
+            defaultTeclas();
+            document.getElementById("flechaDer").src="../static/imgs/flechasON.png";
+            document.getElementById("letraD").style.border="none";
+            document.getElementById("letraD").style.backgroundColor="rgb(131, 131, 131)";
             socket.emit("enviarKeyCode", keyCode)
         }
         else{
@@ -55,14 +77,37 @@ window.onload = function() {
         document.getElementById("luzQ").src="../static/imgs/luzVerdeOff.png";
         document.getElementById("luzE").src="../static/imgs/luzVerdeOff.png";
         document.getElementById("luzS").src="../static/imgs/luzVerdeOff.png";
+        document.getElementById("flechaIzq").src="../static/imgs/flechasOff.png";
+        document.getElementById("flechaDer").src="../static/imgs/flechasOff.png";
     }
 
     function defaultTeclas(){
-        document.getElementById("letraW").style.backgroundColor="white";
-        document.getElementById("letraS").style.backgroundColor="white";
-        document.getElementById("letraE").style.backgroundColor="white";
-        document.getElementById("letraQ").style.backgroundColor="white";
+        document.getElementById("letraW").style.borderRight="2px rgb(87, 87, 87) solid";
+        document.getElementById("letraW").style.borderBottom="2px rgb(87, 87, 87) solid";
+        document.getElementById("letraW").style.backgroundColor="rgb(182, 182, 182)";
+
+        document.getElementById("letraS").style.borderRight="2px rgb(87, 87, 87) solid";
+        document.getElementById("letraS").style.borderBottom="2px rgb(87, 87, 87) solid";
+        document.getElementById("letraS").style.backgroundColor="rgb(182, 182, 182)";
+
+        document.getElementById("letraD").style.borderRight="2px rgb(87, 87, 87) solid";
+        document.getElementById("letraD").style.borderBottom="2px rgb(87, 87, 87) solid";
+        document.getElementById("letraD").style.backgroundColor="rgb(182, 182, 182)";
+
+        document.getElementById("letraA").style.borderRight="2px rgb(87, 87, 87) solid";
+        document.getElementById("letraA").style.borderBottom="2px rgb(87, 87, 87) solid";
+        document.getElementById("letraA").style.backgroundColor="rgb(182, 182, 182)";
+
+        document.getElementById("letraE").style.borderRight="2px rgb(87, 87, 87) solid";
+        document.getElementById("letraE").style.borderBottom="2px rgb(87, 87, 87) solid";
+        document.getElementById("letraE").style.backgroundColor="rgb(182, 182, 182)";
+
+        document.getElementById("letraQ").style.borderRight="2px rgb(87, 87, 87) solid";
+        document.getElementById("letraQ").style.borderBottom="2px rgb(87, 87, 87) solid";
+        document.getElementById("letraQ").style.backgroundColor="rgb(182, 182, 182)";
+
     }
+
 };
 
 
