b4d10fcbb7a3870ee0344f7fc3b152de9d8d999f
arsaca <arsaca2122daw2@protonmail.com>
2022-05-06 16:41:39 +0200

    TODO CSS ACABADO - thumb que no va, qeuda arreglar payload

--- static/js/socketioClient.js
+++ static/js/socketioClient.js
@@ -10,14 +10,16 @@ window.onload = function() {
     window.addEventListener("keyup", (event) =>{
         apagarLuces();
         defaultTeclas();
+        tituloBODYapagar()
+        tituloHEADapagar()
         socket.emit("keyUp")
     })
 
     function movimentKeyMotor(keyCode){
         if(keyCode == 87){// key w
             console.log("Cliente pulsa W")
-            apagarLuces();
             defaultTeclas();
+            tituloBODYencender()
             document.getElementById("luzW").src="../static/imgs/luzVerdeOn.png";
             document.getElementById("letraW").style.border="none";
             document.getElementById("letraW").style.backgroundColor="rgb(131, 131, 131)";
@@ -26,8 +28,8 @@ window.onload = function() {
         }
         else if(keyCode == 83){ //Key s
             console.log("Cliente pulsa S")
-            apagarLuces();
             defaultTeclas();
+            tituloBODYencender()
             document.getElementById("luzS").src="../static/imgs/luzVerdeOn.png";
             document.getElementById("letraS").style.border="none";
             document.getElementById("letraS").style.backgroundColor="rgb(131, 131, 131)";
@@ -36,8 +38,8 @@ window.onload = function() {
         }
         else if(keyCode == 81){ //Key q
             console.log("Cliente pulsa Q")
-            apagarLuces();
             defaultTeclas();
+            tituloHEADencender()
             document.getElementById("luzQ").src="../static/imgs/luzVerdeOn.png";
             document.getElementById("letraQ").style.border="none";
             document.getElementById("letraQ").style.backgroundColor="rgb(131, 131, 131)";
@@ -46,26 +48,24 @@ window.onload = function() {
         }
         else if(keyCode == 69){ //Key e
             console.log("Cliente pulsa E")
-            apagarLuces();
             defaultTeclas();
+            tituloHEADencender()
             document.getElementById("luzE").src="../static/imgs/luzVerdeOn.png";
             document.getElementById("letraE").style.border="none";
             document.getElementById("letraE").style.backgroundColor="rgb(131, 131, 131)";
             let potencia = document.getElementById("sliderMotor22").value;
             socket.emit("enviarKeyCode", keyCode, potencia)
         }
-        else if(keyCode == 65){
+        else if(keyCode == 65){ //Key a
             console.log("Cliente pulsa A")
-            apagarLuces();
             defaultTeclas();
             document.getElementById("flechaIzq").src="../static/imgs/flechasON.png";
             document.getElementById("letraA").style.border="none";
             document.getElementById("letraA").style.backgroundColor="rgb(131, 131, 131)";
             socket.emit("enviarKeyCode", keyCode)
         }
-        else if(keyCode == 68){
+        else if(keyCode == 68){//Key d
             console.log("Cliente pulsa D")
-            apagarLuces();
             defaultTeclas();
             document.getElementById("flechaDer").src="../static/imgs/flechasON.png";
             document.getElementById("letraD").style.border="none";
@@ -111,6 +111,22 @@ window.onload = function() {
         document.getElementById("letraQ").style.borderBottom="2px rgb(87, 87, 87) solid";
         document.getElementById("letraQ").style.backgroundColor="rgb(182, 182, 182)";
 
+    }
+
+    function tituloHEADencender(){
+        document.getElementById("motor2Titulo").style.opacity="1";
+    }
+
+    function tituloBODYencender(){
+        document.getElementById("motor1Titulo").style.opacity="1";
+    }
+
+    function tituloHEADapagar(){
+        document.getElementById("motor2Titulo").style.opacity="0.8";
+    }
+
+    function tituloBODYapagar(){
+        document.getElementById("motor1Titulo").style.opacity="0.8";
     }
 
 };
